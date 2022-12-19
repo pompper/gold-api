807affcbfb0584a01e2f2fcf3697ea73eb8fb6a8
Born Scream <pompper@hotmail.com>
2022-12-19 22:18:14 +0700

    pup

--- index.js
+++ index.js
@@ -10,36 +10,41 @@ app.set('view engine', 'ejs')
 app.use(express.static("public")) //Serv img/css  files
 
 app.get('/', (request, response) => {
-  // getGold(response);  
-  response.send("OK")
+  getGold(response);  
+  // response.send("OK")
 })
 
-// async function getGold(response){
-//     const URL = "https://www.goldtraders.or.th/default.aspx";
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto(URL);
+async function getGold(response){
+    const URL = "https://www.goldtraders.or.th/default.aspx";
+    const browser = await puppeteer.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    });
+    const page = await browser.newPage();
+    await page.goto(URL);
 
-//     //Assign Value
-//     barSell = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblBLSell");
-//     barSellPrice = await page.evaluate((barSell) => barSell.textContent, barSell);
-//     barBuy = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblBLBuy");
-//     barBuyPrice = await page.evaluate((barBuy) => barBuy.textContent, barBuy);
-//     jewSell = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblOMSell");
-//     jewSellPrice = await page.evaluate((jewSell) => jewSell.textContent, jewSell);
-//     jewBuy = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblOMBuy");
-//     jewBuyPrice = await page.evaluate((jewBuy) => jewBuy.textContent, jewBuy);
+    //Assign Value
+    barSell = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblBLSell");
+    barSellPrice = await page.evaluate((barSell) => barSell.textContent, barSell);
+    barBuy = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblBLBuy");
+    barBuyPrice = await page.evaluate((barBuy) => barBuy.textContent, barBuy);
+    jewSell = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblOMSell");
+    jewSellPrice = await page.evaluate((jewSell) => jewSell.textContent, jewSell);
+    jewBuy = await page.waitForSelector("#DetailPlace_uc_goldprices1_lblOMBuy");
+    jewBuyPrice = await page.evaluate((jewBuy) => jewBuy.textContent, jewBuy);
 
-//      let today = new Date()
-//      let thMonth = ['มกราคม', 'กุมภาพันธ์','มีนาคม', 'เมษายน', 'พฤษภาคม','มิถุนายน', 'กรกฏาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม','พฤศจิกายน', 'ธันวาคม']
-//        response.render("index", {
-//          barSell: barSellPrice,
-//          barBuy: barBuyPrice,
-//          jewBuy: jewBuyPrice,
-//          jewSell: jewSellPrice,
-//          date: today.getDate() + " " + thMonth[today.getMonth()] + " " + parseInt(today.getFullYear() + 543),
-//        });  
-// }
+     let today = new Date()
+     let thMonth = ['มกราคม', 'กุมภาพันธ์','มีนาคม', 'เมษายน', 'พฤษภาคม','มิถุนายน', 'กรกฏาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม','พฤศจิกายน', 'ธันวาคม']
+       response.render("index", {
+         barSell: barSellPrice,
+         barBuy: barBuyPrice,
+         jewBuy: jewBuyPrice,
+         jewSell: jewSellPrice,
+         date: today.getDate() + " " + thMonth[today.getMonth()] + " " + parseInt(today.getFullYear() + 543),
+       });  
+}
 
 // app.listen(PORT);
 // console.log('Server started on port 80')
