ede7e270ff5faf965b996d1c45d554e58e3ebdcb
Born Scream <pompper@hotmail.com>
2022-12-20 12:25:20 +0700

    try

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const app = require("express")();
-// const express = require("express");
+const express = require("express");
 // const ejs = require("ejs");
 
 let chrome = {};
 let puppeteer;
 
 // app.set('view engine', 'ejs')
-// app.use(express.static("public")) //Serv img/css  files
+app.use(express.static("public")) //Serv img/css  files
 // app.use(express.static("public")); //Serv img/css  files
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
@@ -53,7 +53,45 @@ app.get("/", async (req, res) => {
     let today = new Date()
    let thMonth = ['มกราคม', 'กุมภาพันธ์','มีนาคม', 'เมษายน', 'พฤษภาคม','มิถุนายน', 'กรกฏาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม','พฤศจิกายน', 'ธันวาคม']
 // res.setHeader("Content-Type","text/html")
-    res.send("<h1>"+jewSellPrice+"</h1>");
+    // res.send("<h1>"+jewSellPrice+"</h1>");
+    res.send(`
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gold API by 7/7 House</title>
+    <!-- Canvas -->
+    <script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main>
+        <div id="capture">
+            <span id="date"><%= date %></span>
+            <span id="custom" contenteditable="true">25,300</span>
+            <span><%= barSell %></span><br>
+            <span id="barbuy"><%= barBuy %></span><br>
+            <span id="jewsell"><%= jewSell %></span> <br>
+            <span id="jewbuy"><%= jewBuy %> </span><br>
+        </div>
+        <p>กด Save แล้ว คลิกขวาที่ รูปภาพ แล้วเซฟอีกครั้ง</p>
+        <button onclick="capture()">Save</button>
+    </main>
+    <section></section>
+<script>
+    function capture(){
+        html2canvas(document.querySelector("#capture")).then(canvas => {
+            document.getElementsByTagName('body')[0].appendChild(canvas)
+            document.getElementsByTagName('main')[0].remove()
+        });
+    }
+</script>
+</body>
+</html>
+    `);
     // res.render("index", {
     // //   barSell: barSellPrice,
     // //   barBuy: barBuyPrice,
